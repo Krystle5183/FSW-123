01e6c47fcf9d9c0c61fc38ffe4550695de8a7342
Krystle5183 <96323148+Krystle5183@users.noreply.github.com>
2022-07-14 13:37:24 -0500

    Add files via upload

---
+++ week5/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GiphySearch from './GiphySearch.js';
+
+function App() {
+  return (
+    <GiphySearch initial = {"Welcome"}/>
+  )
+}
+
+export default App;
+
+//res.data[0].images.original.url
